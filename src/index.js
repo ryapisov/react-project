a62800f3fc7cb0383d397e12bdd3ef5af4bbf98e
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-10-09 17:52:08 +0300

    Added a new project structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from 'routes'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+const App = () =>{
+  return (
+    <div>
+      <h3>Hooks</h3>
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  )
+}
 
 ReactDOM.render( 
   <React.StrictMode >
