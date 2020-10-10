9ad5f746affef7f1aad99d9499ecc9dd3d9c8fa9
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-10-10 18:52:42 +0300

    Added context for  currentUser

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom'
 import Routes from 'routes'
 import {BrowserRouter as Router} from 'react-router-dom'
 import NavBar from 'components/NavBar'
+import { CurrentUserProvider } from 'contexts/currentUser'
 
 const App = () =>{
   return (
-    <div>
+    <CurrentUserProvider>
       <Router>
         <NavBar />
         <Routes />
       </Router>
-    </div>
+    </CurrentUserProvider>
   )
 }
 
-ReactDOM.render( 
-  <React.StrictMode >
+ReactDOM.render(
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
